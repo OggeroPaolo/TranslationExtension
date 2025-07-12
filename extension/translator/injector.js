c332237a4aa94c095aea25b5f5a98ad22f8a42d8
OggeroPaolo <oggero.paolo@gmail.com>
2025-07-12 18:07:39 +0200

    folder change and renamed main extension file

--- extension/translator/injector.js
+++ extension/translator/injector.js
@@ -3,7 +3,7 @@ console.log("✅ Injector loaded on:", window.location.href);
 
 const script = document.createElement('script');
 script.type = 'module';
-script.src = chrome.runtime.getURL('borderify.js');
+script.src = chrome.runtime.getURL('translator.js');
 (document.head || document.documentElement).appendChild(script);
 script.onload = () => script.remove();
 
